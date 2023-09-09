730c1edc515499c0516cd22e99f6bb52938794b6
Jairus Bondoc <jairus@nmgresources.ph>
2023-09-09 20:23:44 +0800

    modals

---
+++ src/CBandit/BaseCI3-forms/_page-modals-add.php
@@ -0,0 +1,83 @@
+<!-- Modal Delete Prompt -->
+<div tabindex="-1" role="dialog" class="modal fade md-fullColor mod-delete x-mod-delete" data-keyboard="false" data-backdrop="static">
+	<div class="modal-dialog modal-full-color">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" data-dismiss="modal" aria-hidden="true" class="close"><i class="icon s7-close"></i></button>
+				<h3 class="modal-title">Delete Record</h3>
+			</div>
+			<div class="modal-body">
+				<div class="text-center">
+					<ext-center></ext-center>
+					<div class="i-circle primary"><i class="icon s7-close"></i></div>
+					<h4></h4>
+					<p>Are you sure you want to delete <span class="delete-name"></span> ?</p>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" data-dismiss="modal" class="btn btn-primary btn-shade1 md-fullColor-cancel">Cancel</button>
+				<button type="button" data-dismiss="modal" class="btn btn-primary btn-shade1 md-fullColor-proceed">Proceed</button>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- Modal Edit Success Prompt -->
+<div tabindex="-1" role="dialog" style="" class="modal fade mod-success x-mod-success" data-keyboard="false" data-backdrop="static">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<!--
+				<button type="button " data-dismiss="modal" aria-hidden="true" class="close"><i class="icon s7-close"></i></button>
+				-->
+			</div>
+			<div class="modal-body">
+				<div class="text-center">
+					<div class="i-circle text-success"><i class="icon s7-check"></i></div>
+					<h4></h4>
+					<p>Changes has been saved successfully!</p>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" data-dismiss="modal" class="btn btn-success btn-okay">OK</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div tabindex="-1" role="dialog" style="" class="modal fade mod-success x-mod-success-delete" data-keyboard="false" data-backdrop="static">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button " data-dismiss="modal" aria-hidden="true" class="close"><i class="icon s7-close"></i></button>
+			</div>
+			<div class="modal-body">
+				<div class="text-center">
+					<div class="i-circle text-success"><i class="icon s7-check"></i></div>
+					<h4></h4>
+					<p>Record has been deleted successfully!</p>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" data-dismiss="modal" class="btn btn-success btn-okay">OK</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div tabindex="-1" role="dialog" style="" class="modal fade mod-error" data-keyboard="false" data-backdrop="static">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button " data-dismiss="modal" aria-hidden="true" class="close"><i class="icon s7-close"></i></button>
+			</div>
+			<div class="modal-body">
+				<div class="text-center">
+					<div class="i-circle text-danger"><i class="icon s7-close"></i></div>
+					<h4></h4>
+					<p></p>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" data-dismiss="modal" class="btn btn-danger btn-okay">OK</button>
+			</div>
+		</div>
+	</div>
+</div>
